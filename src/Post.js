ba6aa3b2542c2cc279d166de93279998a24eb368
RyomaDKishimoto <shi-sa-sa-@hotmail.co.jp>
2020-09-30 01:18:36 +0900

    udpate

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            {/* header -> avator + username */}
+            <h3>username</h3>
+            {/* image */}
+
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
